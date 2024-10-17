a85913c08cfe138575ea8501a35db25b2f163346
G0bigit <162537631+G0bigit@users.noreply.github.com>
2024-10-17 12:03:02 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull g0bigit/simple-python-flask-app
+docker pull g0bidocker/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 g0bigit/simple-python-flask-app
+docker run -d -p 5000:5000 g0bidocker/simple-python-flask-app
